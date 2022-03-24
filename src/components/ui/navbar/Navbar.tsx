5a25daec0077ac1b3d0cbdf8595aea858daf2289
barrilocho <barrilocho@gmail.com>
2022-03-23 22:51:41 -0600

    feat(navbar): layout navbar

--- src/components/ui/navbar/Navbar.tsx
+++ src/components/ui/navbar/Navbar.tsx
@@ -1,11 +1,23 @@
 import { Spacer, Text, useTheme } from '@nextui-org/react';
+import Image from 'next/image';
+import { useMemo } from 'react';
 import styles from './NavbarStyles.module.scss';
 
 export const NavbarComponent = () => {
   const { theme } = useTheme();
+  const randomInteger = useMemo(() => Math.floor(Math.random() * 151) + 1, []);
   return (
-    <div className={styles.navbar__container}>
-      <Text color="white" h2>
+    <div
+      className={styles.navbar__container}
+      style={{ backgroundColor: theme?.colors.gray900.value }}
+    >
+      <Image
+        width={60}
+        alt="Icono Pokemon finder"
+        height={60}
+        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${randomInteger}.svg`}
+      ></Image>
+      <Text className={styles.navbar__title} color="white" h2>
         P
       </Text>
       <Text color="white" h3>
