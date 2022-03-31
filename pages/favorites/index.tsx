c6df3f35742b069bd5818cf0014311be81d7d91c
Alejandro Barrios <barrilocho@gmail.com>
2022-03-30 22:40:26 -0600

    feat(favorites): list of favorites added

--- pages/favorites/index.tsx
+++ pages/favorites/index.tsx
@@ -1,14 +1,36 @@
-import { Container, Image, Text } from '@nextui-org/react';
+import { Container, Grid, Image, Text } from '@nextui-org/react';
 import { NextPage } from 'next';
+import { useEffect, useState } from 'react';
 
 import { MainLayout } from '../../src/components/layouts';
+import { PokemonCardComponent } from '../../src/components/ui';
 import { NoFavoritesComponent } from '../../src/components/ui/no-favorites/NoFavoritesComponent';
+import { localFavorites } from '../../src/utils';
 
 const FavoritesPage: NextPage = () => {
+  const [favoritePokemons, setFavoritePokemons] = useState<number[]>([]);
+  useEffect(() => {
+    setFavoritePokemons(localFavorites.pokemons());
+  }, []);
+
   return (
     <>
       <MainLayout title="Favorites pokemons">
-        <NoFavoritesComponent />
+        {!favoritePokemons.length ? (
+          <NoFavoritesComponent />
+        ) : (
+          <Grid.Container gap={2} direction="row" justify="flex-start">
+            {favoritePokemons.map((id) => (
+              <PokemonCardComponent
+                pokemon={{
+                  img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`,
+                  id: id,
+                }}
+                key={id}
+              ></PokemonCardComponent>
+            ))}
+          </Grid.Container>
+        )}
       </MainLayout>
     </>
   );
