386689b415a770b95b5b98bca935bcf5534228ef
Alejandro Barrios <barrilocho@gmail.com>
2022-04-01 01:15:02 -0600

    feat(add og tags): add tags for seo

--- src/components/layouts/main-layout/MainLayout.tsx
+++ src/components/layouts/main-layout/MainLayout.tsx
@@ -5,6 +5,7 @@ import { NavbarComponent } from '../../ui/navbar/Navbar';
 interface Props {
   title: string;
 }
+const origin = typeof window === 'undefined' ? '' : window.location.origin;
 
 export const MainLayout: FC<Props> = ({ children, title }) => {
   return (
@@ -12,8 +13,20 @@ export const MainLayout: FC<Props> = ({ children, title }) => {
       <Head>
         <title>{title || 'Pokémon Finder'} </title>
         <meta name="author" content="Alejandro Barrios" />
-        <meta name="description" content={`Pokemon finder application, ${title}`} />
-        <meta name="keywords" content={`${title}, Pokemon, Pokedex, Pokemon finder`} />
+        <meta
+          name="description"
+          content={`Pokemon finder application, ${title}`}
+        />
+        <meta
+          name="keywords"
+          content={`${title}, Pokemon, Pokedex, Pokemon finder`}
+        />
+        <meta property="og:title" content={`Información sobre ${title}`} />
+        <meta
+          property="og:description"
+          content={`Esta es la páginasobre ${title}`}
+        />
+        <meta property="og:image" content={`${origin}/img/banner.jpg`} />
       </Head>
       <NavbarComponent />
       <main>{children}</main>
