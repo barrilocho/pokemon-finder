8f69e638679450f6fdf6791102bdb51a4e64de9c
Alejandro Barrios <barrilocho@gmail.com>
2022-04-01 00:51:21 -0600

    refactor(api): optimize pages with necessary data of pokemon

--- pages/name/[name].tsx
+++ pages/name/[name].tsx
@@ -115,10 +115,14 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { name } = params as { name: string };
   const { data } = await pokeApi.get<PokemonInterface>(`/pokemon/${name}`);
-
+  const pokemon = {
+    id: data.id,
+    name: data.name,
+    sprites: data.sprites,
+  };
   return {
     props: {
-      pokemon: data,
+      pokemon,
     },
   };
 };
--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -117,10 +117,14 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { id } = params as { id: string };
 
   const { data } = await pokeApi.get<PokemonInterface>(`/pokemon/${id}`);
-
+  const pokemon = {
+    id: data.id,
+    name: data.name,
+    sprites: data.sprites,
+  };
   return {
     props: {
-      pokemon: data,
+      pokemon,
     },
   };
 };
